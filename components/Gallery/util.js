8be1e3e96e53134c246c99e72dcf32d165ce75c9
Yongkyun Shin <yongkyuns@gmail.com>
2022-04-06 20:44:45 -0400

    add dummy function in gallery/util

--- components/Gallery/util.js
+++ components/Gallery/util.js
@@ -1,7 +1,5 @@
 import * as THREE from "three";
 import { proxy } from "valtio";
-import { Spinner, Box } from "@chakra-ui/react";
-import { forwardRef } from "react";
 
 export const damp = THREE.MathUtils.damp;
 export const state = proxy({
@@ -11,38 +9,4 @@ export const state = proxy({
   ),
 });
 
-export const GalleryContainer = forwardRef(({ children }, ref) => (
-  <Box
-    ref={ref}
-    className="voxel-dog"
-    m="auto"
-    mt={["-20px", "-60px", "-120px"]}
-    mb={["-40px", "-140px", "-200px"]}
-    w={[280, 480, 640]}
-    h={[280, 480, 640]}
-    position="relative"
-  >
-    {children}
-  </Box>
-));
-
-export const GallerySpinner = () => (
-  <Spinner
-    size="xl"
-    position="absolute"
-    left="50%"
-    top="50%"
-    ml="calc(0px - var(--spinner-size) / 2)"
-    mt="calc(0px - var(--spinner-size))"
-  />
-);
-
-const Loader = () => {
-  return (
-    <GalleryContainer>
-      <GallerySpinner />
-    </GalleryContainer>
-  );
-};
-
-export default Loader;
+export default function Dummy() {}
